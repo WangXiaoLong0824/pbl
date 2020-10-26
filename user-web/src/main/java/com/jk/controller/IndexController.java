e7b19b0098f71969342ad60dbc535b5b841db204
zhangjiufeng <1944044590@qq.com>
2020-10-26 17:38:17 +0800

    前台页面

---
+++ user-web/src/main/java/com/jk/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.jk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("index")
+    public String toindex(){
+        return "index";
+    }
+}
