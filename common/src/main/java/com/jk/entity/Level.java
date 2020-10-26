8981e1cc0e819887afd12125e88668878cc0d530
WangXiaoLong0824 <980187827@qq.com>
2020-10-26 16:50:33 +0800

    213

---
+++ common/src/main/java/com/jk/entity/Level.java
@@ -0,0 +1,9 @@
+package com.jk.entity;
+
+import lombok.Data;
+
+@Data
+public class Level {
+    private Integer levelId;
+    private String levelName;
+}
--- common/src/main/java/com/jk/entity/User.java
+++ common/src/main/java/com/jk/entity/User.java
@@ -8,4 +8,23 @@ public class User {
 
     private String userName;
 
+    private Integer userSex;
+
+    private String userPhone;
+
+    private String userEmail;
+
+    private String createTime;
+
+    private String userJiFen;
+
+    private Integer levelId;
+
+    private String userStatus;
+
+    //业务
+    private String levelName;
+
+
+
 }
